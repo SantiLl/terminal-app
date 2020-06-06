8478f97273f8c9e3be8575e6bdf8706a0c093fcc
Santiago Llapur <santillapur@gmail.com>
2020-06-06 20:11:19 -0300

    feat[files]: Add options if file does not exist

--- controllers/files_controller.rb
+++ controllers/files_controller.rb
@@ -8,21 +8,26 @@ class FilesController
   end
 
   def create_file(name, content)
-    p name
-    p content
     new_file = File.new(name: name, content: content)
     @files_repository.post(new_file)
   end
 
   def show_file(file)
     searched_file = @files_repository.get(file)
-    @file_view.display_file(searched_file)
+    if searched_file
+      @file_view.display_file(searched_file)
+    else
+      @file_view.not_found(file)
+    end
   end
 
   def destroy_file(file)
     searched_file = @files_repository.get(file)
-    @files_repository.delete(searched_file)
-    @file_view.deleted_file
+    if @files_repository.delete(searched_file)
+      @file_view.delete_file
+    else
+      @file_view.not_found(file)
+    end
   end
 
   def display_file_helper
--- views/file_view.rb
+++ views/file_view.rb
@@ -1,6 +1,6 @@
 class FileView
   def display_file(file)
-    puts "id: #{file.id} - name: #{file.name} - content: #{file.content}"
+    puts "File: ( name: #{file.name} | content: #{file.content} )"
   end
 
   def file_helper
@@ -11,7 +11,11 @@ class FileView
     puts "4. 'destroy_file filename'"
   end
 
-  def deleted_file
-    puts 'File deleted successfully!'
+  def delete_file(file)
+    puts "File '#{file}' deleted successfully!"
+  end
+
+  def not_found(file)
+    puts "#{file}: No such file."
   end
 end
