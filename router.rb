677e6d5991894b3e25508042c33b52c1ee9c071d
Santiago Llapur <santillapur@gmail.com>
2020-06-14 22:53:16 -0300

    refact[router]: Change files and folder actions

--- router.rb
+++ router.rb
@@ -24,7 +24,7 @@ class Router
   def actions(answer)
     if answer.include?('file')
       file_actions(answer)
-    elsif answer.include?('folder') || answer.include?('cd') || answer.include?('ls')
+    elsif answer.include?('folder') || answer.include?('cd') || answer.include?('ls') || answer.include?('whereami')
       folder_actions(answer)
     elsif answer.include?('help')
       command_helper
@@ -35,44 +35,53 @@ class Router
 
   def file_actions(answer)
     file_command = answer.split(' ')
-    if file_command[0].include?('show_file') && file_command.size == 2
+    case [file_command[0], file_command.size]
+    when ['show_file', 2]
       @files_controller.show_file(file_command[1], @opened_folders[-1])
-    elsif file_command[0].include?('metadata_file') && file_command.size == 2
+    when ['metadata_file', 2]
       @files_controller.metadata_file(file_command[1], @opened_folders[-1])
-    elsif file_command[0].include?('create_file') && file_command.size >= 3
+    when ['create_file', file_command.size]
+      joined_content = file_command[2] if file_command.size == 2
       joined_content = join_content(file_command) if file_command.size > 2
       @files_controller.create_file(file_command[1], joined_content, @opened_folders[-1])
-    elsif file_command[0].include?('destroy_file') && file_command.size == 2
+    when ['destroy_file', 2]
       @files_controller.destroy_file(file_command[1], @opened_folders[-1])
-    elsif file_command[0] == 'file' && file_command[1] == '-h'
-      @files_controller.display_file_helper
+    when ['file', 2]
+      @files_controller.display_file_helper if file_command[1] == '-h'
+      invalid_operation(answer, 'file') unless file_command[1] == '-h'
     else
-      puts "Invalid operation '#{answer}', use file -h to check all the file commands"
+      invalid_operation(answer, 'file')
     end
   end
 
   def folder_actions(answer)
     folder_command = answer.split(' ')
-    if folder_command[0].include?('create_folder') && folder_command.size == 2
+    case [folder_command[0], folder_command.size]
+    when ['create_folder', 2]
       @folders_controller.create_folder(folder_command[1], @opened_folders[-1])
-    elsif folder_command[0].include?('destroy_folder') && folder_command.size == 2
+    when ['destroy_folder', 2]
       @folders_controller.destroy_folder(folder_command[1], @opened_folders[-1])
-    elsif folder_command[0] == 'folder' && folder_command[1] == '-h'
-      @folders_controller.display_folder_helper
-    elsif folder_command[0].include?('cd') && folder_command.size == 2
+    when ['cd', 2]
       redirect_folder(folder_command[1])
-    elsif folder_command[0] == 'ls' && folder_command.size == 1
+    when ['ls', 1]
       @folders_controller.display_all(@opened_folders[-1])
-    elsif folder_command[0] == 'whereami' && folder_command.size == 1
+    when ['whereami', 1]
       @folders_controller.check_location(@opened_folders)
+    when ['folder', 2]
+      @folders_controller.display_folder_helper if folder_command[1] == '-h'
+      invalid_operation(answer, 'folder') unless folder_command[1] == '-h'
     else
-      puts "Invalid operation '#{answer}', use folder -h to check all the file commands"
+      invalid_operation(answer, 'folder')
     end
   end
 
   def command_helper
     puts "1. For file commands: 'file -h'"
     puts "2. For folder commands: 'folder -h'"
+  end
+
+  def invalid_operation(answer, object)
+    puts "Invalid operation '#{answer}', use #{object} -h to check all the #{object} commands"
   end
 
   def join_content(collection)
@@ -89,7 +98,6 @@ class Router
     elsif folder.include?('..') && @opened_folders.size.positive?
       @folders_controller.close_folder(@opened_folders[-1])
       @opened_folders.pop
-      puts "Closed folder in : #{@opened_folders}"
     else
       run_in_folder(folder) if @folders_controller.check_folder(folder, @opened_folders[-1])
     end
@@ -99,7 +107,6 @@ class Router
     if !@opened_folders.include?(folder)
       @folders_controller.open_folder(folder)
       @opened_folders << folder
-      puts "Added folder in : #{@opened_folders}"
     else
       puts 'Folder already opened!'
     end
